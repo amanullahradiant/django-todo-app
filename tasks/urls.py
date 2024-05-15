2670d68aae79faadfa4a61e3c82cbebbf81cc4e8
amanullahradiant <amanullah.radiant@gmail.com>
2024-05-15 14:25:21 +0600

    update task done

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
-    path("createPost", views.createPost),
+    path("createPost", views.createPost, name="list"),
+    path("update_task/<str:pk>/", views.updateTask, name="update_task"),
 ]
