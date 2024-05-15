df56e70b12205fc06672ddedd2fe680bd0f43d94
amanullahradiant <amanullah.radiant@gmail.com>
2024-05-15 11:43:08 +0600

    created and registered model and rendered tasks in the view template

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+
 
 # Create your views here.
+def index(request):
+    # return HttpResponse("hello world!")
+
+    tasks = Task.objects.all()
+    context = {"tasks": tasks}
+    return render(request, "tasks/list.html", context)
