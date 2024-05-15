3c81df67f07358bc0edac1c9516c2838994d05d5
amanullahradiant <amanullah.radiant@gmail.com>
2024-05-15 13:51:29 +0600

    done basic add task functionality

--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
 from .models import *
+from .forms import *
+import json
 
 
 # Create your views here.
 def index(request):
     # return HttpResponse("hello world!")
+    createTaskForm = TaskForm()
 
     tasks = Task.objects.all()
-    context = {"tasks": tasks}
+
+    context = {"tasks": tasks, "createTaskForm": createTaskForm}
     return render(request, "tasks/list.html", context)
+
+
+def createPost(request):
+    if request.method == "POST":
+        # data = {"method": request.method, "POST": request.POST.dict()}
+        # return JsonResponse(data)
+
+        form = TaskForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("/")
